4f2093b29a16a12754e8eecd17517517719a5365
ComePicard <come.picard@gmail.com>
2025-04-25 13:41:55 +0200

    refactor() replace age with date_of_birth

--- app/db/init.sql
+++ app/db/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    age INT CHECK (age >= 0),
+    date_of_birth DATE,
     address VARCHAR(255),
     phone_number VARCHAR(15) UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
