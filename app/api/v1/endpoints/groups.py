516696b0636f5dce8cb96bd55f182391d64b1e93
Lucas Sarazin <sarazin.lucas4@gmail.com>
2025-04-25 14:48:44 +0200

    feat() added crud for groups

---
+++ app/api/v1/endpoints/groups.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter
+
+from app.schemas.groups import Group, GroupCreate
+from app.services.groups import fetch_all_groups, remove_group, fetch_group_by_id, create_group, edit_group
+
+router = APIRouter()
+
+
+@router.get(path="/")
+async def get_groups() -> list[Group]:
+    """
+    Affiche les utilisateurs stockés dans la BDD.
+    """
+    return await fetch_all_groups()
+
+@router.get(path="/{group_id}")
+async def get_group(group_id: str) -> Group:
+    """
+    Affiche un utilisateur stocké dans la BDD.
+    """
+    return await fetch_group_by_id(group_id)
+
+
+@router.post('/')
+async def post_group(group: GroupCreate) -> Group:
+    """
+    Crée un utilisateur dans la BDD.
+    """
+    return await create_group(group)
+
+@router.patch('/{group_id}')
+async def patch_group(group_id: str, group: GroupCreate) -> Group:
+    """
+    Modifie un utilisateur dans la BDD.
+    """
+    return await edit_group(group_id, group)
+
+
+@router.delete('/{group_id}')
+async def delete_group(group_id: str) -> str:
+    """
+    Supprime un utilisateur dans la BDD.
+    """
+    return await remove_group(group_id)
